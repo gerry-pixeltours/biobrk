66bd322679f9b6888725ae8084470995bd1d6111
gerry-pixeltours <gerry@pixeltours.com>
2014-03-12 18:15:01 -0400

    Fixed the broken test

--- tests/functional.js
+++ tests/functional.js
@@ -1,11 +1,21 @@
 var superagent = require('superagent'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    SUCCESS = 200;
 
 describe('express rest api server', function(){
+  it('responds to the route "/"', function(done){
+    superagent.get('http://localhost:3000/')
+      .end(function(error, result){
+        expect(result.status).to.eql(SUCCESS);
+        done();
+      });
+  });
+
   it('responds to the route "/timer/0001"', function(done){
     superagent.get('http://localhost:3000/timer/0001')
       .end(function(error, result){
-        expect(error).to.eql(null);
-      })
-  })
-});
+        expect(result.status).to.eql(SUCCESS);
+        done();
+      });
+  });
+})
